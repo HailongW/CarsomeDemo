e87f5af36f32e7edac4c2208585ddfe18adbdf91
Hai Long Wang <hailong.wang@carsome.com>
2022-11-13 15:47:45 +0800

    Initial Commit

---
+++ CarsomeDemo/CarsomeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsomeDemoApp.swift
+//  CarsomeDemo
+//
+//  Created by 王海龙 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct CarsomeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarsomeDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CarsomeDemo
+//
+//  Created by 王海龙 on 2022/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
